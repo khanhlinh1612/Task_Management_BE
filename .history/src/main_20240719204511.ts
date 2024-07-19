093f76ede51df35190f1e5fd84353029205ae43e
khanhlinh1612 <linhp069@gmail.com>
2024-07-19 22:45:41 +0700

    FixCrossError

--- .history/src/main_20240719204511.ts
+++ .history/src/main_20240719204511.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',  // Replace with your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
   await app.listen(8080);
 }
 bootstrap();
